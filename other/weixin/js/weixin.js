b98aa161cc7206e4f2eba246447b34996b96d6a2
zhangping1234 <1195039899@qq.com>
2015-07-07 16:28:20 +0800

    微信页面调整1
    微信页面调整1

--- other/weixin/js/weixin.js
+++ other/weixin/js/weixin.js
@@ -21,7 +21,23 @@ $(function(){
 })
 
 
+/*-----------w_suiyou 随游---------------*/
 
+$(function(){
+	$('.w_suiyou .selects .a1').click(function(e) {
+        $('.syprop01').css('display','block');
+        $('.syprop02,.syprop03').css('display','none');
+    });
+	$('.w_suiyou .selects .a2').click(function(e) {
+        $('.syprop02').css('display','block');
+        $('.syprop01,.syprop03').css('display','none');
+    });
+	$('.w_suiyou .selects .a3').click(function(e) {
+        $('.syprop03').css('display','block');
+        $('.syprop01,.syprop02').css('display','none');
+    });
+
+})
 
 
 
